b57ce2e284fb5e44692178f95d688aad0cf534e6
favric <fcvagliente@gmail.com>
2016-11-15 14:30:57 -0300

    MigrateCommentsUpdated

--- database/migrations/2016_11_15_170609_post_usuarios.php
+++ database/migrations/2016_11_15_170609_post_usuarios.php
@@ -18,6 +18,7 @@ class PostUsuarios extends Migration
          $table->integer('user_id')->unsigned();
         $table->foreign('user_id')->references('id')->on('users');
         $table->string('post_text');
+        $table->integer('append_postid');
         $table->ipAddress('visitor');
         $table->macAddress('device');
         $table->rememberToken();
