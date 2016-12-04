cdb5cc89c5acc845541c0b21f6b62782d744cc5c
favric <fcvagliente@gmail.com>
2016-12-04 07:39:20 -0300

    Arreglado Problema en Seeds + RegisterController

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,7 +17,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->freeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => $password ?: $password = bcrypt('123456'),
         'fecha_de_nacimiento' => $faker->date,
         'genero' => $faker->randomElement($array = array ('M','F')),
         'remember_token' => str_random(10),
@@ -29,7 +29,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 $factory->define(App\Post::class, function (Faker\Generator $faker) {
 
     return [
-        'user_id' => $faker->numberBetween($min = 1, $max = 28),
+        'user_id' => $faker->numberBetween($min = 1, $max = 25),
         'post_text' => $faker->text($maxNbChars = 200) ,
         'group_id' => $faker->randomElement($array = array ('1','2','3','4','5','6')),
         'remember_token' => str_random(10),
