c11a59f7fdb102b213dd3d3be3470103ce3c7579
favric <fcvagliente@gmail.com>
2016-12-04 22:36:41 -0300

    Mod.Json

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         Cviebrock\EloquentSluggable\ServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
         Cviebrock\EloquentSluggable\ServiceProvider::class,
+        Hootlex\Friendships\FriendshipsServiceProvider::class,
         /*
          * Package Service Providers...
          */
